169c994b49cf7f9ce474336902494324286ae3ec
daominhquan176 <daominhquan176@gmail.com>
2018-05-15 17:18:32 +0700

    cập nhật
    tổng hợp file liên quan Đồ án thầy thiện

---
+++ QLHN/QuanLyHocNghe/QuanLyHocNghe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuanLyHocNghe.Startup))]
+namespace QuanLyHocNghe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
